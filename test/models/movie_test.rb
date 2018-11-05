1f29efec2ea709250bd11c4ccbf9beadff70a20d
Anibel America <ameriabel@gmail.com>
2018-11-05 15:24:18 -0800

    adds movie model validation tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,84 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  # let(:movie) { Movie.new }
+  #
+  # it "must be valid" do
+  #   value(movie).must_be :valid?
+  # end
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  describe 'validations' do
+
+    let(:movie_data) {
+      {
+        movie: {
+          title: 'The Grinch',
+          overview: 'The Grinch and his loyal dog, Max, live a solitary existence inside a cave on Mount Crumpet.',
+          release_date: Date.parse("2017-11-06"),
+          inventory: 2
+        }
+      }
+
+    }
+
+    it 'successfully creates a new movie when given valid title, overview, release_date, and inventory' do
+      old_count = Movie.count
+      movie = Movie.new(movie_data[:movie])
+
+      movie.save
+
+      movie.valid?.must_equal true
+      (Movie.count).must_equal old_count + 1
+      (movie.title).must_equal movie_data[:movie][:title]
+      (movie.overview).must_equal movie_data[:movie][:overview]
+      (movie.release_date).must_equal movie_data[:movie][:release_date]
+      (movie.inventory).must_equal movie_data[:movie][:inventory]
+    end
+
+    it 'does not create a new movie when missing a title' do
+      old_count = Movie.count
+      movie_data[:movie][:title] = nil
+      movie = Movie.new(movie_data[:movie])
+
+      movie.valid?.must_equal false
+      (Movie.count).must_equal old_count
+    end
+
+    it 'does not create a new movie when missing an overview' do
+      old_count = Movie.count
+      movie_data[:movie][:overview] = nil
+      movie = Movie.new(movie_data[:movie])
+
+      movie.valid?.must_equal false
+      (Movie.count).must_equal old_count
+    end
+
+    it 'does not create a new movie when missing a release_date' do
+      old_count = Movie.count
+      movie_data[:movie][:release_date] = nil
+      movie = Movie.new(movie_data[:movie])
+
+      movie.valid?.must_equal false
+      (Movie.count).must_equal old_count
+    end
+
+    it 'does not create a new movie when missing an inventory' do
+      old_count = Movie.count
+      movie_data[:movie][:inventory] = nil
+      movie = Movie.new(movie_data[:movie])
+
+      movie.valid?.must_equal false
+      (Movie.count).must_equal old_count
+    end
+
+    it 'does not create a new movie when inventory is given as a non-integer' do
+      old_count = Movie.count
+      movie_data[:movie][:inventory] = "Not a number"
+      movie = Movie.new(movie_data[:movie])
+
+      movie.valid?.must_equal false
+      (Movie.count).must_equal old_count
+    end
   end
+
 end
