0a1057b8d6c282ba55e2d6d989f3b70a2ec9a4ad
Madaleine Shields <maddiev.shields@gmail.com>
2018-11-06 15:35:27 -0800

    Write tests for index

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -12,29 +12,34 @@ describe MoviesController do
     return body
   end
 
-  it 'is a real working route and returns JSON' do
-    movie = Movie.first
-    movie_id = movie.id
-    # binding.pry
-    get movie_path(movie_id), as: :json
-    body = parse_json(expected_type: Hash)
-    # binding.pry
+  describe 'index' do
+    it 'should list all customers in JSON and renders a status code of success' do
+      get customers_path, as: :json
 
-    expect(body.keys.sort).must_equal MOVIE_FIELDS
+      body = parse_json(expected_type: Array)
+
+      expect(body.length).must_equal Movie.count
+    end
   end
 
-  it 'returns JSON with an error message and status code of not found if pet does not exist' do
-    movie = Movie.first
-    movie_id = movie.id
-    movie.destroy
+  describe 'show' do
+    it 'is a real working route and returns JSON' do
+      movie = Movie.first
+      movie_id = movie.id
+      get movie_path(movie_id), as: :json
+      body = parse_json(expected_type: Hash)
+      expect(body.keys.sort).must_equal MOVIE_FIELDS
+    end
 
-    #Act
-    get movie_path(movie_id), as: :json
+    it 'returns JSON with an error message and status code of not found if pet does not exist' do
+      movie = Movie.first
+      movie_id = movie.id
+      movie.destroy
 
-    #Assert
-    body = parse_json(expected_type: Hash, expected_status: :not_found)
-
-    expect(body["errors"]).must_include "movie_id"
+      get movie_path(movie_id), as: :json
+      body = parse_json(expected_type: Hash, expected_status: :not_found)
+      expect(body["errors"]).must_include "movie_id"
+    end
   end
 
 end
