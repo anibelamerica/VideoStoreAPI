284c9c29e8d08d053d22a10889e17581f5b859bc
Anibel America <ameriabel@gmail.com>
2018-11-06 14:21:41 -0800

    adds movies controller show action tests and methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,29 @@
 class MoviesController < ApplicationController
+
+  before_action :find_movie, only: [:show]
+
   def index
   end
 
   def show
+    if @movie
+      render json: jsonify(@movie)
+    else
+      render_error(:not_found, { movie_id: ["Movie does not exist"] })
+    end
   end
 
   def create
+  end
+
+  private
+
+  def find_movie
+    @movie = Movie.find_by(id: params[:id])
+  end
+
+  def jsonify(movie_data)
+    return movie_data.as_json(only: [:title, :overview, :release_date, :inventory] )
   end
 
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  MOVIE_FIELDS = %w(title overview release_date inventory).sort
+
+  def parse_json(expected_type:, expected_status: :success)
+    must_respond_with expected_status
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
+  end
+
+  it 'is a real working route and returns JSON' do
+    movie = Movie.first
+    movie_id = movie.id
+
+    get movie_path(movie_id)
+
+    body = parse_json(expected_type: Hash)
+
+    expect(body.keys.sort).must_equal MOVIE_FIELDS
+  end
+
+  it 'returns JSON with an error message and status code of not found if pet does not exist' do
+    movie = Movie.first
+    movie_id = movie.id
+    movie.destroy
+
+    #Act
+    get movie_path(movie_id)
+
+    #Assert
+    body = parse_json(expected_type: Hash, expected_status: :not_found)
+
+    expect(body["errors"]).must_include "movie_id"
+  end
+
 end
