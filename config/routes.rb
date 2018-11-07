8fa866bfa693601bdb9f4f4f1298ec35c45396b8
Madaleine Shields <maddiev.shields@gmail.com>
2018-11-07 10:44:33 -0800

    Add routes for rentals

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'rentals/create'
-  get 'rentals/update'
+  post 'rentals/check-out', to:'rentals#create', as: 'check-out'
+
+  patch 'rentals/check-in', to: 'rentals#update', as: 'check-in'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/zomg', to: 'application#test', as: 'test'
 
   resources :movies, only: [:index, :show, :create]
   resources :customers, only: [:index, :show]
-  
+
+
+
 end
