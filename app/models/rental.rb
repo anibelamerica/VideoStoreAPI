377b5039c6fb1d1f100d49dcae0115364d8dbf6f
Madaleine Shields <maddiev.shields@gmail.com>
2018-11-07 10:34:37 -0800

    Add helper methods to models and def create/update in controller for Rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,17 @@
 class Rental < ApplicationRecord
+  ONE_WEEK = 7
+
+  validates :checked_out?, inclusion: { in: [true, false] }
+
+  belongs_to :customer
+  belongs_to :movie
+
+  def self.assign_due_date
+    checkout_date = self.created_at.to_date
+    self.update(due_date: (checked_out + ONE_WEEK))
+  end
+
+  def self.check_in_movie
+    self.update(checked_out?: false)
+  end
 end
