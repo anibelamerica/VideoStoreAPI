5513f4464e035fa7fe4838ad2e000484c9e18596
Anibel America <ameriabel@gmail.com>
2018-11-07 06:42:33 -0800

    includes tests for overdue rentals endpoint

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,14 +1,38 @@
 require "test_helper"
 
 describe RentalsController do
-  it "should get create" do
-    get rentals_create_url
-    value(response).must_be :success?
+
+  RENTAL_FIELDS = %w(movie_id title customer_id name postal_code checkout_date due_date)
+
+  def parse_json(expected_type:, expected_status: :success)
+    must_respond_with expected_status
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
   end
 
-  it "should get update" do
-    get rentals_update_url
-    value(response).must_be :success?
+  describe 'overdue' do
+    it 'returns a list of all customers with overdue books in JSON with a status code of success' do
+      #Arrange
+      # TODO create rentals in yml files with overdue parameters, or
+      # update yml rentals to change checked_out? to true (as long as date is
+      # more than 1 week ago)
+
+      get overdue_rentals_path
+
+      body = parse_json(expected_type: Array)
+
+      expect(body.keys.sort).must_equal MOVIE_FIELDS
+    end
+
+    it 'returns an empty array with a status code of success if there are no customers with overdue books' do
+      get overdue_rentals_path
+
+      body = parse_json(expected_type: Array)
+      expect(body).must_equal []
+    end
   end
 
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,37 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  let(:rental) { Rental.first }
+  #
+  # it "must be valid" do
+  #   value(rental).must_be :valid?
+  # end
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+  describe 'due_date' do
+    it 'returns the date for which this instance of rental is due' do
+      expected_due_date = rental.created_at + 7
+
+      expect(rental.due_date).must_equal expected_due_date
+    end
   end
+
+  describe 'overdue?' do
+    it 'returns true if this instance of rental is overdue' do
+      #Arrange
+      #TODO: update an existing rental so that it's overdue
+      #rental = Rental.first
+      #patch rentals_path(rental.id)
+
+      expect(rental.overdue?).must_equal true
+    end
+
+    it 'returns false if this instance of rental is not overdue' do
+      #Arrange
+      #TODO: create a new rental or grab one that's definitely not overdue
+      #rental = Rental.create
+
+      expect(rental.overdue?).must_equal false
+    end
+  end
+
 end
