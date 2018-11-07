3b65f336617ff80547f265e48cfe8a57dc5b0b58
Madaleine Shields <maddiev.shields@gmail.com>
2018-11-07 10:36:19 -0800

    Add relationships to rentals

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ class Movie < ApplicationRecord
   validates :overview, presence: true
   validates :release_date, presence: true
   validates :inventory, presence: true, numericality: true
+
+  has_many :rentals
 end
